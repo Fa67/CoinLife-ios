3749bed4c9068226a5aaf3f40d3764264cc5089d
ser U <User@Userui-MacBook.local>
2017-10-10 16:06:46 +0900

    Initial Commit

---
+++ CoinPrice/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CoinPrice
+//
+//  Created by User on 2017. 10. 10..
+//  Copyright © 2017년 jungho. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
